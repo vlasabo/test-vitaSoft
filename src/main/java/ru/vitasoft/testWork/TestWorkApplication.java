a0a5b4a8c47810383af7635f079e3c84f540cf31
Vladimir <vladimirsabo@yandex.ru>
2023-03-30 14:35:59 +0300

    feat: добавить зависимости, gitignore, application.yml

---
+++ src/main/java/ru/vitasoft/testWork/TestWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.vitasoft.testWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWorkApplication.class, args);
+    }
+
+}
